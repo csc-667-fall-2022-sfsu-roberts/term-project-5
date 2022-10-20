b500c6acf8e02cd12b44da358636d365f12483e2
Brian Cheng <briancheng@sfs-wifi-dhcp-10-143-121-228.wifi.sfsu.edu>
2022-10-20 15:01:24 -0700

    Added routes for login and registration

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (request, response) => {
+  response.render('landing', { title: 'Express' });
 });
+
+router.get('/login', (request, response) => {
+  response.render('login', { title: 'Login' });
+})
+
+router.get('/registration', (request, response) => {
+  response.render('registration', {title: 'Registration'});
+})
+
 
 module.exports = router;
