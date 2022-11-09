ed27d42a10363a55cebc7ba6100c0853f7a350f2
Brian Cheng <briancheng@Brians-MacBook-Pro.local>
2022-11-09 09:26:25 -0800

    Initialize Sign Up

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signup', (request, response) => {
+  /* IMPLEMENT SIGN UP */
+})
 
 module.exports = router;
